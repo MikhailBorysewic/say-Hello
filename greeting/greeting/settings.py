1fdf70ee1d91b489b3bc92ed0f18597b30aedf55
Mikhail Borysewic <mikhail.borysewic@gmail.com>
2022-11-18 15:28:41 +0300

    Update settings.py

--- greeting/greeting/settings.py
+++ greeting/greeting/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sc$s9ym74n+3qdjfbvt3hqjd4h+el+qplufy_hx1)d7n9%lj+7'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', False) is not False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['andrejgru.pythonanywhere.com', 'andrejgru.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
